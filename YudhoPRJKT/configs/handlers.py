a04968eb6ef74b8b91f772d9e0d464ac4978a0ea
YudhoPatrianto <kydh01123@gmail.com>
2025-12-23 19:08:56 +0700

    config: fix token not pasted in api
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- YudhoPRJKT/configs/handlers.py
+++ YudhoPRJKT/configs/handlers.py
@@ -1,43 +1,46 @@
 from dotenv import load_dotenv
 import os
 
-
 class Config:
   def __init__(self) -> None:
     self.envfile = f'{os.getcwd()}/.env'
-    load_dotenv(self.envfile)
-
-    # Telegram bot properties
-    self._token = os.environ.get('token')
-    self._api = f'https://api.telegram.org/bot{self._token}'
-    if not self._token:
-      self._token = str(input('Input your telegram bot token: '))
+    # Telegram bot credetials
+    self._api = None
     if not os.path.exists(self.envfile):
+      token = str(input('Input your telegram bot api: '))
+      self._token = token
       with open(self.envfile, 'w') as w:
-        if open(self.envfile, 'r').read() == '':
-          w.write(f"token='{self._token}'\napi='{self._api}'\n")
-    if self._token != '' and self._api  != '':
-      self._token = os.environ.get('token', self._token)
-      self._api = os.environ.get('api', self._api)
-  
-  def UseCustomApi(self, custom_api: str):
-    self.api = f'{custom_api}/bot{self._token}'
+        w.write(f"token='{token}'")
+        if self._api == '':
+          self._api = f'https://api.telegram.org/bot{token}'
+        else:
+          pass
+        load_dotenv(self.envfile)
+    else:
+      load_dotenv(self.envfile)
+      self._token = os.environ.get('token')
+      self._api = os.environ.get('api')
+  def UseCustomApi(self, custom_api):
+    """Custom Api Wrapper
+
+    Args:
+        custom_api (str): Your Custom API
+    """
+    self._api = f'{custom_api}/bot{self._token}'
+    with open(self.envfile, 'w') as w:
+      w.write(f"token='{self._token}'\napi='{self._api}'")
     return self
   
-  # Api
-  @property
-  def api(self):
-    return self._api
-  
-  @api.setter
-  def api(self, api):
-    self._api = api
-  
-  # Token
   @property
   def token(self):
     return self._token
-  
   @token.setter
   def token(self, token):
     self._token = token
+  
+  @property
+  def api(self):
+    return self._api
+  @api.setter
+  def api(self, api):
+    self._api = api
